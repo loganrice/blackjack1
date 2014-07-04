f7e0b6e76741d0c7fbaecf7b2d230ad15c6bbd6b
Logan Rice <loganrice72@gmail.com>
2014-07-04 11:45:38 -0700

    changed deck to account for ace

--- main.rb
+++ main.rb
@@ -7,12 +7,31 @@ def initialize_deck
 
   suits.each do |suit|
     ranks.each do |rank|
-      key = (suit + rank.to_s).to_sym
-      key = "#{rank} of #{suit}".to_sym
-      deck[key] = rank 
+      key = "#{rank} of #{suit}"
+      if rank.is_a? String and rank != "ace"
+        deck[key] = 10
+      else
+        deck[key] = rank 
+      end
     end
   end
+
   return deck 
 end
 
-puts initialize_deck
+def deal(deck)
+  card = deck.to_a.sample
+end
+
+def black_jack
+  deck = initialize_deck
+  player_hand = []
+  computer_hand = []
+  2.times { player_hand << deal(deck) }
+  2.times { computer_hand << deal(deck) }
+  binding.pry
+end
+
+black_jack
+  
+
